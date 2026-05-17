d697f633f31d1a63a783b97dfd8e4141a29b18b0
ItsSam <itssmnle@gmail.com>
2026-05-17 19:45:44 +0700

    Implement ScrollToTop component to reset page scroll on route transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
 import LandingPage from './pages/LandingPage';
 import ModsPage from './pages/ModsPage';
 import HistoryPage from './pages/HistoryPage';
 
+// Helper component that resets page scroll to (0, 0) on route transition
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/mods" element={<ModsPage />} />
